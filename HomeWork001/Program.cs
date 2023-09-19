816f7e242dfe5124d76b53c62b31eba03e9dfd1a
AndreevAV <koriolanrus@gmail.com>
2023-09-19 13:49:46 +0300

    Добавил блок ввода чисел для первой задачи

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -1,2 +1,8 @@
 ﻿//Example 1
 Console.Clear();
+Console.WriteLine("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int numberC = Convert.ToInt32(Console.ReadLine());
