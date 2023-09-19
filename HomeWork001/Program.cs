3606efefaf7aa4e0c4fac65b0997c4c5f7d00a81
AndreevAV <koriolanrus@gmail.com>
2023-09-19 14:00:09 +0300

    Завершил первую задачу

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -1,8 +1,18 @@
 ﻿//Example 1
-Console.Clear();
-Console.WriteLine("Введите первое число:");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число:");
-int numberB = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье число:");
-int numberC = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.WriteLine("Введите первое число:");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// if (numberA > numberB)
+// {
+// Console.WriteLine("Максимум - " + numberA);
+// Console.WriteLine("Минимум - " + numberB);
+// }
+// else if (numberA < numberB)
+// {
+// Console.WriteLine("Максимум - " + numberB);
+// Console.WriteLine("Минимум - " + numberA);
+// }
+// else
+// Console.WriteLine("Числа равны!");
