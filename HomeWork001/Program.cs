f281ef51b9b6979593da968e490742d10d4078d5
AndreevAV <koriolanrus@gmail.com>
2023-09-19 14:18:47 +0300

    Завершил первую задачу 2

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -11,8 +11,23 @@
 // }
 // else if (numberA < numberB)
 // {
-// Console.WriteLine("Максимум - " + numberB);
-// Console.WriteLine("Минимум - " + numberA);
+// Console.WriteLine("Max - " + numberB);
+// Console.WriteLine("Min - " + numberA);
 // }
 // else
 // Console.WriteLine("Числа равны!");
+
+//Example 2
+// Console.Clear();
+// Console.WriteLine("Введите первое число:");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число:");
+// int numberC = Convert.ToInt32(Console.ReadLine());
+// if ((numberA >= numberB) && (numberA >= numberC))
+// Console.WriteLine("Максимальное число: " + numberA);
+// else if ((numberB >= numberA) && (numberB >= numberC))
+// Console.WriteLine("Максимальное число: " + numberB);
+// else 
+// Console.WriteLine("Максимальное число: " + numberC);
