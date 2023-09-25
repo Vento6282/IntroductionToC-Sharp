7b91536e5156db7eecb851bed2e09f49e93421c2
AndreevAV <koriolanrus@gmail.com>
2023-09-25 16:33:26 +0300

    HomeWork002

---
+++ HomeWork002/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Console.WriteLine("Введите трёхзначное число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Вторая цифра числа {number} - '{number / 10 % 10}'");
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// int temp = number;
+// while (temp != 0)
+// {
+//     temp = temp / 10;
+//     count = count + 1;
+// }
+// if (count >= 3)
+// {
+//     Console.WriteLine($"Третья цифра числа {number} - {((number - (number % Math. Pow(10,count - 3))) / Math. Pow(10,count - 3)) % 10}");
+// }
+// else
+//     Console.WriteLine("Введено число, в котором меньше трёх разрядов!");
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// string [] week = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+// Console.WriteLine("Введите день недели:");
+// int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+// if(dayOfWeek > 0 && dayOfWeek <=7)
+// {
+//     if(week[dayOfWeek -1] == "Saturday" || week[dayOfWeek -1] == "Sunday")
+//         Console.WriteLine("Это выходной день!");
+//     else
+//         Console.WriteLine("Это рабочий день.");       
+// }
+// else
+//     Console.WriteLine("Дня недели с таким номером не существует!");    
+
+
+
+
+
+
