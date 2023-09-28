d1ec9c0f6b3d8ab16e546c158904d7306848d1ab
AndreevAV <koriolanrus@gmail.com>
2023-09-28 16:33:57 +0300

    Add HomeWork003

---
+++ HomeWork003/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+// Внутри класса Answer напишите метод IsPalindrome, который принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+// Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+// Для остальных чисел вернуть True или False.
+// bool IsPalindrome(int number)
+// {
+//       if(number > 9999 && number < 100000)
+//       {
+//         if (((number % 100) + (number / 1000)) % 11 == 0)
+//           return true;
+//         return false;
+//       }
+//       else
+//       {
+//         Console.WriteLine("Число не пятизначное");
+//         return false;
+//       }
+// }
+// Console.WriteLine("Введите пятизначное число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(number);
+// Console.WriteLine(IsPalindrome(number));
